ff62449b6549a5dfbf147c2ef77858f58080c113
Alysa Maranan <marananalysa029@gmail.com>
2026-09-15 16:04:08 +0800

    Remove database cache from quail breed helper

--- app/Helpers/QuailBreedHelper.php
+++ app/Helpers/QuailBreedHelper.php
@@ -4,62 +4,67 @@ namespace App\Helpers;
 
 use App\Models\QuailBreed;
 use App\Models\FarmSetting;
-use Illuminate\Support\Facades\Cache;
 
 class QuailBreedHelper
 {
     public static function getCurrentBreed()
     {
-        return Cache::remember('current_quail_breed', 3600, function() {
-            $breedId = FarmSetting::get('current_breed_id', '1');
-            return QuailBreed::find($breedId) ?? QuailBreed::first();
-        });
+        $breedId = FarmSetting::get('current_breed_id', '1');
+
+        return QuailBreed::find($breedId) ?? QuailBreed::first();
     }
 
     public static function setCurrentBreed($breedId)
     {
         FarmSetting::set('current_breed_id', $breedId);
-        Cache::forget('current_quail_breed');
-        Cache::forget('current_quail_breeds');
     }
 
     // Get both selected breeds
     public static function getCurrentBreeds()
     {
-        return Cache::remember('current_quail_breeds', 3600, function() {
-            $breedIds = json_decode(FarmSetting::get('current_breed_ids', json_encode([1, 1])), true);
-            $breeds = [];
-            
-            foreach ($breedIds as $breedId) {
-                $breed = QuailBreed::find($breedId);
-                if ($breed) {
-                    $breeds[] = $breed;
-                }
+        $breedIds = json_decode(
+            FarmSetting::get(
+                'current_breed_ids',
+                json_encode([1, 1])
+            ),
+            true
+        );
+
+        $breeds = [];
+
+        foreach ($breedIds as $breedId) {
+            $breed = QuailBreed::find($breedId);
+
+            if ($breed) {
+                $breeds[] = $breed;
             }
-            
-            // If no breeds found, return default
-            if (empty($breeds)) {
-                $default = QuailBreed::first();
-                return [$default, $default];
-            }
-            
-            return $breeds;
-        });
+        }
+
+        // If no breeds found, return default
+        if (empty($breeds)) {
+            $default = QuailBreed::first();
+
+            return [$default, $default];
+        }
+
+        return $breeds;
     }
 
     // Set both selected breeds
     public static function setCurrentBreeds($breedId1, $breedId2)
     {
         $breedIds = [$breedId1, $breedId2];
-        FarmSetting::set('current_breed_ids', json_encode($breedIds));
-        Cache::forget('current_quail_breeds');
-        Cache::forget('current_quail_breed');
+
+        FarmSetting::set(
+            'current_breed_ids',
+            json_encode($breedIds)
+        );
     }
 
     public static function getBreedInfo($field = null)
     {
         $breed = self::getCurrentBreed();
-        
+
         if (!$breed) {
             return $field ? null : [];
         }
