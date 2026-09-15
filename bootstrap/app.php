b5d463fac63099b46bfef194dd37db16ab023ddb
Alysa Maranan <marananalysa029@gmail.com>
2026-09-15 14:27:20 +0800

    Fix Throwable warning in bootstrap

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -3,7 +3,6 @@
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
-use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -20,7 +19,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withExceptions(function (Exceptions $exceptions): void {
 
         // TEMPORARY: Send Laravel exceptions directly to Render logs
-        $exceptions->report(function (Throwable $e) {
+        $exceptions->report(function (\Throwable $e) {
             error_log(
                 'Q-MATE ERROR: ' .
                 get_class($e) .
